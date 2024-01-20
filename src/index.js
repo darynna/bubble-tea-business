85a677814601b237a4999b7322308f51449f4075
Daryna Holikova <dgolikova0@gmail.com>
2024-01-20 11:09:31 +0000

    css of header

---
+++ src/index.js
@@ -0,0 +1,60 @@
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
+
+
+
+const goTopBtn = document.querySelector(".go-top");
+
+window.addEventListener("scroll", trackScroll);
+
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+
+  const scrolled = window.pageYOffset;
+
+  const coords = document.documentElement.clientHeight;
+
+  if (scrolled > coords) {
+
+    goTopBtn.classList.add("go-top--show");
+  } else {
+
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+function goTop() {
+
+  if (window.pageYOffset > 0) {
+  
+    window.scrollBy(0, -25); //другий аргумент - це швидкість скролу
+    setTimeout(goTop, 0); 
+  }
+}
